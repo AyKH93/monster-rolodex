6aa91e7dcd89f98816ac975a2fc73f5a945de6da
Aykh93 <ay.khelifi@hotmail.com>
2019-08-01 22:19:23 +0100

    Updates

---
+++ precache-manifest.128edc81fd529403f31a4d35711bd0e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd99dcebb18b2c060092fd729708a12e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e3183c5c38a2f697b76f",
+    "url": "/monster-rolodex/static/css/main.71965947.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e3183c5c38a2f697b76f",
+    "url": "/monster-rolodex/static/js/main.27b84d11.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
